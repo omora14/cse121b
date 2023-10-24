5b16fa330f144284bec8fd6f63f0787221f96b7c
omora14 <owenmorales9@gmail.com>
2023-10-23 23:47:12 -0600

    Changes

--- scripts/data.js
+++ scripts/data.js
@@ -1,6 +1,6 @@
 // Module to fetch external data
 export function fetchData() {
-    // Replace this URL with your own data source
+  
     const dataURL = 'https://run.mocky.io/v3/feed470f-81ad-4a8e-8c5c-5c72753276bd';
   
     return fetch(dataURL)
