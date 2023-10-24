c983546999854a79f7e535f3b798066f8b59d3bd
omora14 <owenmorales9@gmail.com>
2023-10-24 00:11:17 -0600

    Comments

--- scripts/data.js
+++ scripts/data.js
@@ -1,6 +1,6 @@
 // Module to fetch external data
 export function fetchData() {
-  
+  // I decided to use the mock since my work has nothing to do with the API I found.
     const dataURL = 'https://run.mocky.io/v3/feed470f-81ad-4a8e-8c5c-5c72753276bd';
   
     return fetch(dataURL)
